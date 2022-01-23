b74fe0d3a7c5d9cf024bd3e2f01c4108e2634fbc
tnvsu <tnvsu@Pandu.myfiosgateway.com>
2022-01-23 18:14:12 -0500

    Used switch statement to print the interest rates.

---
+++ src/main/java/Sudha/Datatype.java
@@ -0,0 +1,29 @@
+package Sudha;
+
+public class Datatype {
+	public static void main(String args[]) {
+	int govtroi=1;
+			int chaseroi=2;
+			int unitedroi=15;
+			int suntrustroi=7;
+			
+
+		switch (suntrustroi) {
+		case 1:
+			System.out.println("Personal interest from the bank govtroi 10 " +govtroi);
+		case 2:
+			System.out.println("Personal interest from the bank chaseroi 11 " +chaseroi);
+		case 3:
+			System.out.println("Personal interest from the bank unitedroi 12 " +unitedroi);
+		case 4:
+			System.out.println("Personal interest from the bank suntrust 13 " +suntrustroi);
+		
+		default:
+			System.out.println("Personal interest from the bank govtroi 10 " +govtroi);
+
+		}
+
+	}
+}
+	
+
