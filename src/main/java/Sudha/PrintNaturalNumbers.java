47016df50d779b87bf1de12b119826c61f098080
tnvsu <tnvsu@Pandu.myfiosgateway.com>
2022-01-23 18:46:43 -0500

    Assignment day 5

---
+++ src/main/java/Sudha/PrintNaturalNumbers.java
@@ -0,0 +1,13 @@
+package Sudha;
+
+public class PrintNaturalNumbers {
+
+	public static void main(String args[]) {
+		int N = 1000000;
+		int i = 1;
+
+		System.out.println("Print the numbers" + N);
+		for (i = 0; i <= N; i++)
+		System.out.println(i + "");
+	}
+}
